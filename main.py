d412f23f16abdb8b4e03a5d12d42350a30eae423
임성우 <seongwoorang@naver.com>
2020-09-22 23:33:12 +0900

    Update main.py
    dp로 풀이

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ n=int(input())
 
 array=list(map(int,input().split()))
 
-result=0
-for i in range(n):
-    for j in range(i+2,n):
-        result=max(result,array[i],array[i]+array[j])
+d=[0]*100       #dp 테이블 초기화
 
-print(result)
+
+#d[n]은 n번째에서 털 수 있는 최대 식량의 값
+d[0]=array[0]
+d[1]=max(array[0],array[1])
+for i in range(2,n):
+    d[i]=max(d[i-1],d[i-2]+array[i])
+
+print(d[n-1])
